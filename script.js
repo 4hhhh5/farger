8d25b18dd1c1f52628f16f883d70b818140cb86c
4hhhh5 <iris.epost@gmail.com>
2026-04-21 20:31:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,145 @@
+const asvalt = document.querySelector(`#asvalt`);
+const blå = document.querySelector(`#blå`);
+const cyan = document.querySelector(`#cyan`);
+const demin = document.querySelector(`#demin`);
+const eplegrønn = document.querySelector(`#eplegrønn`);
+const fiolett = document.querySelector(`#fiolet`);
+const grønn = document.querySelector(`#grønn`);
+const hvit = document.querySelector(`#hvit`);
+const indigo = document.querySelector(`#indigo`);
+const jade = document.querySelector(`#jade`);
+const kobloblå = document.querySelector(`#kobloblå`);
+const lilla = document.querySelector(`#lilla`);
+const magenta = document.querySelector(`#magenta`);
+const nattblå = document.querySelector(`#nattblå`);
+const oransje = document.querySelector(`#oransje`);
+const plomme = document.querySelector(`#plomme`);
+const quartz = document.querySelector(`#quartz`);
+const rød = document.querySelector(`#rød`);
+const svart = document.querySelector(`#svart`);
+const turkis = document.querySelector(`#turkis`);
+const ultramarin = document.querySelector(`#ultramarin`);
+const vinrød = document.querySelector(`#vinrød`);
+const wenge = document.querySelector(`#wenge`);
+const xanthic = document.querySelector(`#xanthic`);
+const ynk = document.querySelector(`#ynk`);
+const zaffre = document.querySelector(`#zaffre`);
+const ærfuglblå = document.querySelector(`#ærfuglblå`);
+const ørkenbrun = document.querySelector(`#ørkenbrun`);
+const åkergul = document.querySelector(`#åkergul`);
+
+asfalt.addEventListener(`keydown`, (a) => {
+    document.body.style.backgroundColor = "#3b3a30";
+});
+
+blå.addEventListener(`keydown`, (b) => {
+    document.body.style.backgroundColor = "#1a3b5c";
+});
+
+cyan.addEventListener(`keydown`, (c) => {
+    document.body.style.backgroundColor = "#f7e7ce";
+});
+
+denim.addEventListener(`keydown`, (d) => {
+    document.body.style.backgroundColor = "#c08081";
+});
+
+eplegrønn.addEventListener(`keydown`, (e) => {
+    document.body.style.backgroundColor = "#44d7a8";
+});
+
+fiolett.addEventListener(`keydown`, (f) => {
+    document.body.style.backgroundColor = "#fc8eac";
+});
+
+grønn.addEventListener(`keydown`, (g) => {
+    document.body.style.backgroundColor = "#ffd700";
+});
+
+hvit.addEventListener(`keydown`, (h) => {
+    document.body.style.backgroundColor = "#e3d3b0";
+});
+
+indigo.addEventListener(`keydown`, (i) => {
+    document.body.style.backgroundColor = "#282c34";
+});
+
+jade.addEventListener(`keydown`, (j) => {
+    document.body.style.backgroundColor = "#29ab87";
+});
+
+koboltblå.addEventListener(`keydown`, (k) => {
+    document.body.style.backgroundColor = "#d1b399";
+});
+
+lilla.addEventListener(`keydown`, (l) => {
+    document.body.style.backgroundColor = "#e6e6fa";
+});
+
+magenta.addEventListener(`keydown`, (m) => {
+    document.body.style.backgroundColor = "#191970";
+});
+
+nattblå.addEventListener(`keydown`, (n) => {
+    document.body.style.backgroundColor = "#ad6d2f";
+});
+
+oransje.addEventListener(`keydown`, (o) => {
+    document.body.style.backgroundColor = "#808000";
+});
+
+plomme.addEventListener(`keydown`, (p) => {
+    document.body.style.backgroundColor = "#005f69";
+});
+
+quartz.addEventListener(`keydown`, (q) => {
+    document.body.style.backgroundColor = "#f7cac9";
+});
+
+rød.addEventListener(`keydown`, (r) => {
+    document.body.style.backgroundColor = "#b7410e";
+});
+
+svart.addEventListener(`keydown`, (s) => {
+    document.body.style.backgroundColor = "#9c9f84";
+});
+
+turkis.addEventListener(`keydown`, (t) => {
+    document.body.style.backgroundColor = "#e2725b";
+});
+
+ultramarin.addEventListener(`click`, (u) => {
+    document.body.style.backgroundColor = "#635147";
+});
+
+vinrød.addEventListener(`click`, (v) => {
+    document.body.style.backgroundColor = "#242145";
+});
+
+wenge.addEventListener(`click`, (w) => {
+    document.body.style.backgroundColor = "#d5691e";
+});
+
+xanthic.addEventListener(`click`, (x) => {
+    document.body.style.backgroundColor = "#738678";
+});
+
+ynk.addEventListener(`click`, (y) => {
+    document.body.style.backgroundColor = "#15317e";
+});
+
+zaffre.addEventListener(`click`, (z) => {
+    document.body.style.backgroundColor = "#7c7d7e";
+});
+
+ærfuglblå.addEventListener(`click`, (æ) => {
+    document.body.style.backgroundColor = "#6d6875";
+});
+
+ørkenbrun.addEventListener(`click`, (ø) => {
+    document.body.style.backgroundColor = "#e3e3e3";
+});
+
+åkergul.addEventListener(`click`, (å) => {
+    document.body.style.backgroundColor = "#4a1012";
+});
